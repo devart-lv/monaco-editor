844fe4f77d00ac3fd6c93b39a436bbf5d556e955
Artis Raugulis <artis@devart.lv>
2023-05-29 21:15:43 +0300

    dist

---
+++ esm/vs/editor/common/services/textResourceConfiguration.js
@@ -0,0 +1,5 @@
+import { createDecorator } from '../../../platform/instantiation/common/instantiation.js';
+export const ITextResourceConfigurationService = createDecorator(
+	'textResourceConfigurationService'
+);
+export const ITextResourcePropertiesService = createDecorator('textResourcePropertiesService');
